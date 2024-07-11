31b053de016c4e87d976d5f21692174d6e87c00f
ravi14333 <ravivarmakk8@gmail.com>
2024-07-11 16:13:44 +0530

    first commit

---
+++ Addition.java
@@ -0,0 +1,8 @@
+class Addition{
+public static void main(String [] args)
+{
+int n=39;
+int n1=454;
+System.out.println(n1+n);
+}
+}
---
+++ Even.java
@@ -0,0 +1,8 @@
+class Even{
+public static void main(String [] args)
+{
+int n=10;
+if(){
+System.out.println("your number is even number");
+}
+}
